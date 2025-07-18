663779f5788d63aec1cd5f0906c7c0870fbdbebc
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2025-07-18 05:58:33 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import LoveComedyPage from './page/Love'
+
+function App() {
+
+  return (
+    <>
+      <LoveComedyPage />
+    </>
+  )
+}
+
+export default App
